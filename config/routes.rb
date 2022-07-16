3cecd06505431399d9909fed0788fba977a428e5
Rajan Ladwa <rladwa@gmail.com>
2022-07-16 16:50:27 +0900

    Routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :dogsittings, only: [ :index, :show, :new, :create ] do
+    resources :bookings, only: :create
+  end
+
+  resources :bookings, only: [ :index, :update, :destroy ]
+
+  namespace :walker do
+    resources :bookings, only: :index
+  end
 end
