16efd7b5c34330194aa3511cf4ea1acdad84c2cf
Rajan Ladwa <rladwa@gmail.com>
2022-07-19 20:39:04 +0900

    bookings controller base done, corrections on dogwalking controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,33 @@
+class BookingsController < ApplicationController
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @dogsitting = Dogsitting.find(params[:dogsitting_id])
+    @booking.dogsitting = @dogsitting
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render 'dogsittings/show'
+    end
+  end
+
+  def update
+    @booking.update(booking_params)
+    redirect_to bookings_path
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to bookings_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:starts_at, :ends_at)
+  end
+end
