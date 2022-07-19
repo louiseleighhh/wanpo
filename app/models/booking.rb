d82df820fb301deea272dff8d058fa3547b7d24b
Sara Jessome <sajessome@gmail.com>
2022-07-19 21:04:14 +0900

    Updated booking.rb

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :dogsitting
+  validates :starts_at, :ends_at, :status, presence: true
+  enum status: { pending: 0, accepted: 1, rejected: 2 }
 end
