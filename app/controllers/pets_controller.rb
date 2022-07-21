46c8d27b0a7df695afe55a020f79706b3be63117
louiseleighhh <louisecapala@gmail.com>
2022-07-21 21:58:13 +0900

    new/create for pets controller + profile page

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,22 +1,24 @@
 class PetsController < ApplicationController
-  class PetsController < ApplicationController
-    def new
-      @pet = Pet.new(params[:pet_id])
-    end
 
-    def create
-      @pet = Pet.new(pet_params)
-      if @pet.save
-        redirect_to pet_path(@pet)
-      else
-        render :new
-      end
-    end
+  def new
+    @pet = Pet.new
+    # @pet = Pet.new(params[:pet_id])
+  end
 
-    private
-
-    def pet_params
-      params.require(:pet).permit(:name, :age, :breed, :size, :comments)
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.user = current_user
+    if @pet.save
+      redirect_to root_path
+    else
+      render :new
     end
   end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :age, :breed, :size, :comments)
+  end
+
 end
