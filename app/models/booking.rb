39e8b52883852ed4a12309386f48384f9b2a4fb3
louiseleighhh <louisecapala@gmail.com>
2022-07-26 20:40:06 +0900

    status of bookings will show + can edit & destroy pending bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,7 @@ class Booking < ApplicationRecord
   belongs_to :dogsitting
   validates :starts_at, :ends_at, :status, presence: true
   enum status: { pending: 0, accepted: 1, rejected: 2 }
+  def pending?
+    status == 'pending'
+  end
 end
